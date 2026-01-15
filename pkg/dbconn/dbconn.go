902e40196aee31f84e7a3abb3e375e753f6e7f0d
fadyHemaya <fadyhemaya5@gmail.com>
2026-01-15 12:40:00 +0200

    increase lock wait

--- pkg/dbconn/dbconn.go
+++ pkg/dbconn/dbconn.go
@@ -42,8 +42,8 @@ type DBConfig struct {
 func NewDBConfig() *DBConfig {
 	return &DBConfig{
 		LockWaitTimeout:          30,
-		InnodbLockWaitTimeout:    3,
-		MaxRetries:               3,
+		InnodbLockWaitTimeout:    30, // Increased from 3 for high-traffic tables
+		MaxRetries:               10, // Increased from 3 for high-contention scenarios
 		MaxOpenConnections:       32,    // default is high for historical tests. It's overwritten by the user threads count + 2 for headroom.
 		RangeOptimizerMaxMemSize: 0,     // default is 8M, we set to unlimited. Not user configurable (may reconsider in the future).
 		InterpolateParams:        false, // default is false
